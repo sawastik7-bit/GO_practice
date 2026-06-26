e5107a8176e7f23bcc8facb0b5f05da74d8f193d
Sawastik Bhullar <sawastik7@gmail.com>
2026-06-26 18:28:15 +0530

    wrote the first progaram in go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sawastik7-bit/GO_practice
+
+go 1.26.4
---
+++ main.go
@@ -0,0 +1,7 @@
+package main  // all code is executed inside this package
+
+import "fmt"  // fmt is package to import methods  like Print, Println
+
+func main(){  // all things run inside this main function just like c++
+	fmt.Println("hello this is my first go program")    // this is the syntax to print something in side console
+}
