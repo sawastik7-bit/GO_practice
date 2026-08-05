c2b428a525881dc4cb9afbb9eaec759c3fb0e783
Sawastik Bhullar <sawastik7@gmail.com>
2026-08-05 17:27:44 +0530

    learnt about conditions

---
+++ conditions/ifelse.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+
+
+	fmt.Println();
+	// age := 18
+
+	// if age >= 18 {
+	// 	fmt.Println("you are an adult");
+	// }else if age<18{
+	// 	fmt.Println("balak ho tum");
+	// }
+
+
+
+	// var role="admin";
+	// var haspermission=true;
+
+	// if role =="admin" || haspermission{
+	// 	fmt.Println("you have this permission");
+	// }
+
+// now we can also  declare variable inside if block 
+
+// if age:=15;age>18{
+// 	fmt.Println("you are adult",age);
+// }else {
+// 	fmt.Println("you are a teenager",age);
+// }
+
+
+/// GO doesnt have ternary operrator , we have to use if else block for that 
+
+}
