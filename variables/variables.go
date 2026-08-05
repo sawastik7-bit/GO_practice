8bfafcf20bf840e9de5f92f8b51f5aaf97806589
Sawastik Bhullar <sawastik7@gmail.com>
2026-08-05 17:29:42 +0530

    learnt about variables

---
+++ variables/variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+func main(){
+
+	var name string="gonaldhog";
+	var name1="dfhgoi";  // this is type inference , go lang automatically type infer the datatype
+	var age=35;  
+
+	name2:="gamingpc" // declaring and assigning at the same time 
+	fmt.Println(name);
+	fmt.Println(name1);
+	fmt.Println(age);
+	fmt.Println(name2);
+}
+
