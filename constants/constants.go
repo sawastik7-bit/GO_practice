0fa57710fb78d74c4bf7115b2b40031be07ed40f
Sawastik Bhullar <sawastik7@gmail.com>
2026-08-05 17:27:57 +0530

    learnt about constants

---
+++ constants/constants.go
@@ -0,0 +1,23 @@
+package main
+
+const AGE=30;  // we can also declare outside the funciton 
+
+//  price:=45  // we cant use := syntax outside the function 
+
+func main(){
+
+	const name ="rajesh"; 
+
+	// name="js"  it will give error as we cant reassign constants with const prefix 
+
+
+	// if you have to give some configs , you can give them to it 
+
+	const (  // 
+		port=5000
+		localhost="localhost"
+	)
+
+
+}
+
