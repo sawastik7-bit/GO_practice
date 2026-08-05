ca932ec89fc0e14ace2cc19396f8bc5066ed35db
Sawastik Bhullar <sawastik7@gmail.com>
2026-08-05 17:27:09 +0530

    leart about arrays

---
+++ Arrays/array.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println();
+	// var nums [5]int
+
+	// fmt.Println(len(nums));
+
+
+	// nums[0]=1;
+
+	// fmt.Println(nums[0]);
+	// fmt.Println(nums); // if no elements initialized to the array , then it will be containing 0 
+
+	// arr:=[3]int{1,2,3};
+
+	// fmt.Println(arr);
+
+
+	// Now a 2d array
+
+	// nums:=[2][2]int{{3,4},{5,6}};
+
+	// fmt.Println(nums);
+
+
+	// We can use fix size if predictable elements in the array 
+
+	// good memory optimization
+	// constant time memory access
+
+}
